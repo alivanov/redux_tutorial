50b48973231569f7cff0771f004ca2cf061638c8
Alexey Ivanov <ivanov@intspirit.com>
2015-12-24 13:43:19 +0300

    Setting Up The Socket.io Client, Receiving Actions From The Server

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,7 @@ import {VotingContainer} from './components/Voting';
 import Router, {Route} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
 import App from './components/App';
 import {ResultsContainer} from './components/Results';
@@ -15,15 +16,11 @@ import {ResultsContainer} from './components/Results';
 require('./style.css');
 
 const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine', '28 Days Later'],
-      tally: {Sunshine: 2}
-    }
-  }
-});
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch({type: 'SET_STATE', state})
+);
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
